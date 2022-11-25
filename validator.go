a6da1af01a23a614a7b9063d44b1cb1525566e96
just-do-halee <just.do.halee@gmail.com>
2022-11-25 21:21:41 +0900

    Add validator.go: IsOneOf

--- validator.go
+++ validator.go
@@ -22,3 +22,14 @@ func IsMinMax(min, max int) func(int) error {
 		return nil
 	}
 }
+
+func IsOneOf[T comparable](values ...T) func(T) error {
+	return func(value T) error {
+		for _, v := range values {
+			if value == v {
+				return nil
+			}
+		}
+		return errors.New("value is not one of " + fmt.Sprint(values))
+	}
+}
